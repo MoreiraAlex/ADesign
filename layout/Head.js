70b186d42bfbb29702cdb1cd6083c754d3cc07a9
MoreiraAlex <celerom100@gmail.com>
2022-10-02 14:02:41 -0300

    Configurando SEO

--- layout/Head.js
+++ layout/Head.js
@@ -6,12 +6,12 @@ export default function Head() {
       <meta http-equiv="content-language" content="pt-br" />
       <meta name="author" content="Alex Moreira" />
       <meta name="reply-to" content="celerom100@gmail.com" />
-      <meta name='description' content='Somos os melhores em.....' />
+      <meta name='description' content='Somos os melhores em aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa' />
       <meta name="keywords" content="amagency, marketing digital, redes sociais, landing page, homepage, site, criação de sites, call action, empresa de site, web design, design, programação, frontend, backand"/>
 
       <meta name="google-site-verification" content="Adnx1WQhPC-oB6FllxAbyAF6sCxqANi9kRZTvaPDbsA" />
 
-      <link rel="canonical" href="http://www.meusite.com.br/index.php" />
+      <link rel="canonical" href="https://www.amagency.ga/" />
       <link rel="icon" href="/favicon.png" />
 
       <title>AMAgency</title>
