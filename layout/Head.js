1cd791574f172b9982de40d58fb2dafc83dc897c
MoreiraAlex <celerom100@gmail.com>
2022-10-02 21:44:37 -0300

    Configurando SEO v2

--- layout/Head.js
+++ layout/Head.js
@@ -5,9 +5,11 @@ export default function Head() {
     <NextHead>
       <meta http-equiv="content-language" content="pt-br" />
       <meta name="author" content="Alex Moreira" />
-      <meta name="reply-to" content="celerom100@gmail.com" />
-      <meta name='description' content='Somos os melhores em aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa' />
-      <meta name="keywords" content="amagency, marketing digital, redes sociais, landing page, homepage, site, criação de sites, call action, empresa de site, web design, design, programação, frontend, backand"/>
+      <meta name="reply-to" content="alex.i.mooreira@gmail.com" />
+
+      <meta name='description' content='Agência de Marketing Digital / Gerenciamento em Redes Sociais / Campanhas Google ADS / Criação de Sites / Planejamento de Marketing Digital'/>
+
+      <meta name="keywords" content="o que é leads no marketing digital, Marketing digital, Agência de marketing, Agência, Fundador, gestor de trafego, Home office, vivendo de home office, amagency, redes sociais, landing page, criação de sites, empresa de criação de sites, design, web design"/>
 
       <meta name="google-site-verification" content="Adnx1WQhPC-oB6FllxAbyAF6sCxqANi9kRZTvaPDbsA" />
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import Head from '../layout/Head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head/>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
