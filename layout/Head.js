e9de59728ec0de0f4abafecda90320413428134a
MoreiraAlex <celerom100@gmail.com>
2022-10-09 22:34:38 -0300

    Update Head.js

--- layout/Head.js
+++ layout/Head.js
@@ -15,15 +15,15 @@ export default function Head() {
       <script async src="https://www.googletagmanager.com/gtag/js?id=G-VEN9Z7QGTN"></script>
       <meta http-equiv="content-language" content="pt-br" />
       <meta name="author" content="Alex Moreira" />
-      <meta name="reply-to" content="alex.i.mooreira@gmail.com" />
+      <meta name="reply-to" content="alexmoreira.dev@gmail.com" />
 
       <meta name='description' content='Agência de Marketing Digital / Gerenciamento em Redes Sociais / Campanhas Google ADS / Criação de Sites / Planejamento de Marketing Digital'/>
 
       <meta name="keywords" content="o que é leads no marketing digital, Marketing digital, Agência de marketing, Agência, Fundador, gestor de trafego, Home office, vivendo de home office, amagency, redes sociais, landing page, criação de sites, empresa de criação de sites, design, web design"/>
 
-      <meta name="google-site-verification" content="HxdktV6nr4Ei-DNn_fjAPh2THEuhqRauoNLlPLxgFgs" />
+      {/* <meta name="google-site-verification" content="HxdktV6nr4Ei-DNn_fjAPh2THEuhqRauoNLlPLxgFgs" /> */}
 
-      <link rel="canonical" href="https://www.amagency.ga/" />
+      <link rel="canonical" href="https://amagency.alexmoreira.dev.br/" />
       <link rel="icon" href="/favicon.png" />
 
       <title>AMAgency</title>
